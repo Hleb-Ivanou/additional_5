f13e41c4a1b57b86c57b8b070548b3881e9374ad
Hleb Ivanou <glebguru13@gmail.com>
2018-02-27 23:20:32 +0300

    Implement function check(str, bracketsConfig)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  var newArr = [];
+  var i = 0;
+
+  for (var k = 0; k < bracketsConfig.length; k++) {
+    newArr[k] = bracketsConfig[k].join('');
+  };
+
+  while ( (str.length > 1) && (i < str.length) ) {
+    var temp = str.substring(i, i + 2);
+    if ( newArr.includes(temp) ) {
+      str = str.replace(temp, '');
+      i = 0;
+    } else {
+      i++
+    }
+  }
+
+  return str ? false : true
 }
